0a76ec43fdd77c053a287364439116711bc6f269
Samyak Jain <samyakjain3305@gmail.com>
2023-01-30 22:23:49 +0530

    Panagram_Check solved

---
+++ Python_Programs/Panagram_Check.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param A : list of strings
+    # @return an integer
+    def solve(self, A):
+        alph = "abcdefghijklmnopqrstuvwxyz"
+        q = "".join(A)
+        q = set(q)
+        q = "".join(sorted(q))
+        if q == alph:
+            return 1
+        else:
+            return 0
