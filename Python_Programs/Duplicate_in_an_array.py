6ae9b95a1b00757acf890d6f8a786b78385aab0a
Samyak Jain <samyakjain3305@gmail.com>
2023-02-02 16:40:29 +0530

    Duplicate_in_an_array solved

---
+++ Python_Programs/Duplicate_in_an_array.py
@@ -0,0 +1,16 @@
+#Leetcode
+#User function Template for python3
+#Love babbar DSA Sheet Question 11 of array
+def dup(arr,n):
+    arr.sort()
+    i=0
+    while(i<n):
+        if arr[i]==arr[i+1]:
+            x=arr[i]
+            break
+        i=i+1
+    return x
+import random 
+n=int(input())
+arr=list(map(int,input().strip().split()))
+print(dup(arr,n))
