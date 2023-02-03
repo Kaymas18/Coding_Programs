35cefdd8df8141fbd01068079f18a6cf4a8d8327
Samyak Jain <samyakjain3305@gmail.com>
2023-02-03 19:26:07 +0530

    Palindrome_String solved

---
+++ Python_Programs/Palindrome_String.py
@@ -0,0 +1,29 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 48 of array
+class Solution:
+	def isPalindrome(self, S):
+		# code here
+		str=""
+		i=0
+        for i in S:
+            str = i + str
+        if str==S:
+            return 1
+        else:
+            return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		S = input()
+		ob = Solution()
+		answer = ob.isPalindrome(S)
+		print(answer)
+
+# } Driver Code Ends
