35872c947d64421b1ac8e3dd76c0d1034cd9ad31
Samyak Jain <samyakjain3305@gmail.com>
2023-02-20 23:24:39 +0530

    93_Count_squares solved

---
+++ LOVE BABBAR SHEET/93_Count_squares.py
@@ -0,0 +1,22 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 93 of array
+class Solution:
+    def countSquares(self, N):
+        # code here 
+        return int(math.sqrt(N-1))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.countSquares(N))
+# } Driver Code Ends
