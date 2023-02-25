e4f18c7cd77d1d832985828f70b76af383df571e
Samyak Jain <samyakjain3305@gmail.com>
2023-02-25 21:04:33 +0530

    14_Merge_intervals solved

---
+++ LOVE BABBAR SHEET/1_ARRAY/14_Merge_intervals.py
@@ -0,0 +1,18 @@
+#Leetcode
+#User function Template for python3
+#Love babbar DSA Sheet Question 14 of array
+from ast import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        x = sorted(intervals, key = lambda i: i[0])
+        li = list()
+
+        for start, end in x:
+            if li and li[-1][1] >= start:
+                li[-1][1] = max(li[-1][1], end)
+            else:
+                li.append([start, end])
+            
+        return li
