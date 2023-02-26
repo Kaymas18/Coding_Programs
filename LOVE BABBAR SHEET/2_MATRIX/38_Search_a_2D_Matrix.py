d6773cbd1b419434fc9f9efd2d4bf05ceb8ce9d0
Samyak Jain <samyakjain3305@gmail.com>
2023-02-26 20:36:24 +0530

    38_Search_a_2D_Matrix solved

---
+++ LOVE BABBAR SHEET/2_MATRIX/38_Search_a_2D_Matrix.py
@@ -0,0 +1,7 @@
+#Leetcode
+#User function Template for python3
+#Love babbar DSA Sheet Question 38 of array
+from ast import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return any(target in row for row in matrix)
