720f9910bb4d2d89d849f170a3ff4cd04f6d4a12
Samyak Jain <samyakjain3305@gmail.com>
2023-02-27 14:07:55 +0530

    36_Median_of_2_Sorted_Arrays_of_Different_Sizes solved

---
+++ LOVE BABBAR SHEET/1_ARRAY/36_Median_of_2_Sorted_Arrays_of_Different_Sizes.py
@@ -0,0 +1,35 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 36 of array
+
+class Solution:
+    def MedianOfArrays(self, array1, array2):
+            ar=array1+array2
+            ar.sort()
+            l=len(ar)
+            if(l%2==0):
+                s=((ar[l//2]+ar[(l//2)-1])/2)
+                if(s%1!=0):
+                    pass
+                else:
+                    s=int(s)
+            else:
+                s=(ar[l//2])
+            return s
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    tcs=int(input())
+
+    for _ in range(tcs):
+        m = input()
+        arr1=[int(x) for x in input().split()]
+        n = input()
+        arr2=[int(x) for x in input().split()]
+        
+        
+        ob = Solution()
+        print(ob.MedianOfArrays(arr1,arr2))
+
+# } Driver Code Ends
