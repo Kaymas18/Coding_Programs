936f167b33416e7b355b954d2d1ffc17465dea86
Samyak Jain <samyakjain3305@gmail.com>
2023-02-27 13:20:12 +0530

    17_Best_Time_to_Buy_and_Sell_Stock solved

---
+++ LOVE BABBAR SHEET/1_ARRAY/17_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,16 @@
+#Leetcode
+#User function Template for python3
+#Love babbar DSA Sheet Question 17 of array
+from ast import List
+class Solution:
+   def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        maxi = 0
+        mini = prices[0]
+        for i in range(1, len(prices)):		
+            maxi = max(maxi, prices[i] - mini)
+            mini = min(mini, prices[i])
+        return maxi
+        
