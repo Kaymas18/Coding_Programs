905e933b86fb1b742a72a543d1fc491cc8ed6e4b
Samyak Jain <samyakjain3305@gmail.com>
2023-02-28 20:33:26 +0530

    35_Find_The_Median

---
+++ LOVE BABBAR SHEET/1_ARRAY/35_Find_The_Median.py
@@ -0,0 +1,29 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 35 of array
+
+class Solution:
+	def find_median(self, v):
+            # Code here
+            l= len(v)
+            a=sorted(v)
+            if l%2==0:
+                     med=(a[(l//2)-1] + a[l//2])//2           
+            else:
+                    med=(a[(l//2)])
+            return med
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		v = list(map(int,input().split())) 
+		ob = Solution();
+		ans = ob.find_median(v)
+		print(ans)
+# } Driver Code Ends
