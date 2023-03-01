66d795dc3082131c288266e2beb7cea018d1439d
Samyak Jain <samyakjain3305@gmail.com>
2023-03-01 23:01:38 +0530

    22_Factorial_of_large_Numbers solved

---
+++ LOVE BABBAR SHEET/1_ARRAY/22_Factorial_of_large_Numbers.py
@@ -0,0 +1,27 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 22 of array
+class Solution:
+    def factorial(self, N):
+        #code here
+        i=1
+        for j in range(1,N+1,1):
+            i*=j
+        return str(i)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.factorial(N);
+        for i in ans:
+            print(i,end="")
+        print()
+    
+# } Driver Code Ends
