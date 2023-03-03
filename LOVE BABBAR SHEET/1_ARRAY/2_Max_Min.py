0cdab352d5455f51d09ab785a31ce97e2a586350
Samyak Jain <samyakjain3305@gmail.com>
2023-03-03 19:53:35 +0530

    2_Max_Min solved

---
+++ LOVE BABBAR SHEET/1_ARRAY/2_Max_Min.py
@@ -0,0 +1,24 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 2 of array
+class Solution:
+    def findSum(self,A,N): 
+        #code here
+        a.sort()
+        s=a[0]+a[n-1]
+        return s
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    ob = Solution()
+    print(ob.findSum(a,n))
+# } Driver Code Ends
