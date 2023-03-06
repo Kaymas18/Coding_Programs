da522b8cdd8e6f98bbe15227bd9a86869f8f6f5e
Samyak Jain <samyakjain3305@gmail.com>
2023-03-06 22:40:46 +0530

    _Searching_in_an_array_where_adjacent_differ_by_at_most_k solved

---
+++ LOVE BABBAR SHEET/4_SEARCHING_&_SORTING/98_Searching_in_an_array_where_adjacent_differ_by_at_most_k.py
@@ -0,0 +1,35 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 98 of array
+
+
+def search (arr, n, x, k) : 
+    #Complete the function
+    i,c=0,0
+    while i<len(arr):
+        if arr[i]==x:
+            c=i
+            return c
+        i=i+1
+    return -1
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+    
+
+for _ in range(0,int(input())):
+    n, k = map(int , input().split())
+    arr = list(map(int, input().strip().split()))
+    x = int(input())
+    ans = search(arr, n, x, k)
+    print(ans)
+
+
+
+
+# } Driver Code Ends
