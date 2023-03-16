4283814ef13e921ef8e95380d3f187f90edb214d
Samyak Jain <samyakjain3305@gmail.com>
2023-03-16 15:29:16 +0530

    65_Longest_Prefix_Suffix solved

---
+++ LOVE BABBAR SHEET/3_STRINGS/65_Longest_Prefix_Suffix.py
@@ -0,0 +1,38 @@
+#GFG
+#User function Template for python3
+#Love babbar DSA Sheet Question 65 of array
+
+class Solution:
+	def lps(self, s):
+		# code here
+            lp=[0]*len(s)
+            prLPS,i=0,1
+            while i < len(s):
+                            if s[prLPS]==s[i]:
+                                lp[i]=prLPS+1
+                                prLPS+=1
+                                i+=1
+                            elif prLPS==0:
+                                lp[i]=0
+                                i+=1
+                                
+                            else:
+                                prLPS = lp[prLPS-1]
+                                    
+            return lp[-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s = input()
+
+		ob = Solution()
+		answer = ob.lps(s)
+		print(answer)
+
+# } Driver Code Ends
